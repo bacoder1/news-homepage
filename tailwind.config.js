5f4644d920ec6cf1e1181b13d81ba54fb8a8d48a
bacoder1 <139050658+bacoder1@users.noreply.github.com>
2023-11-01 12:00:39 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'soft-orange': 'hsl(35, 77%, 62%)',
+        'soft-red': 'hsl(5, 85%, 63%)',
+        'off-white': 'hsl(36, 100%, 99%)',
+        'grayish-blue': 'hsl(233, 8%, 79%)',
+        'dark-grayish-blue': 'hsl(236, 13%, 42%)',
+        'very-dark-blue': 'hsl(240, 100%, 5%)',
+      },
+      boxShadow: {
+        '3xl': '-200px 1px 55px 0px rgba(0,0,0,0.36)',
+      }
+    },
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'inter': 'Inter, sans-serif',
+    },
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+  },
+  plugins: [],
+}
+
